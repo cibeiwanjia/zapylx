ef15ce9ff8e88b75f8171d6e264ba4e1292f53a9
WYH <wyh@WYHdeMacBook-Air.local>
2026-03-01 16:11:13 +0800

    chore: fix example code to use all imports

--- example/main.go
+++ example/main.go
@@ -11,8 +11,15 @@ func main() {
 	logger.InitWithDefault()
 
 	logger.Info("使用默认配置启动")
-	logger.Debug("这是调试信息")
+	logger.Debugf("当前时间: %s", time.Now().Format("2006-01-02 15:04:05"))
 	logger.Warn("这是警告信息")
+
+	// 记录带有字段的日志
+	logger.Info("记录详细信息",
+		zap.String("service", "example"),
+		zap.Int("count", 100),
+	)
+
 	logger.Errorf("这是错误信息: %s", "测试错误")
 
 	logger.Sync()
